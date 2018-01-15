4c345e82e0263ff305c62b2ae28619849688bb70
Benedict Lau <ben.hy.lau@gmail.com>
2018-01-15 23:13:22 +0100

    Package yggdrasil-go for armhf and amd64

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,4 +15,4 @@ tar -C /usr/local -xzf /tmp/golang/go1.9.2.linux-amd64.tar.gz
 
 # Copy build scripts from Synced Folder to Vagrant machine user home
 cp -r /vagrant/src/* /home/vagrant/
-chown vagrant:vagrant /home/vagrant/*
+chown -R vagrant:vagrant /home/vagrant/*

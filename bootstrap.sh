f06dad879a75a9c39182805111c959927e020875
Benedict Lau <ben.hy.lau@gmail.com>
2018-01-27 04:35:53 +0100

    Add docker to mesh-orange image

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install standard tools
-apt install -y build-essential git apache2 vim
+apt install -y apt-transport-https build-essential git apache2 vim
 
 # Install golang
 mkdir /tmp/golang

59d371f1c0f8a3d0f087b7f445882f237d215098
Benedict Lau <ben.hy.lau@gmail.com>
2018-12-12 18:58:24 -0500

    Update to nodejs8

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,7 +22,7 @@ tar -C /usr/local -xzf /tmp/golang/go1.11.2.linux-amd64.tar.gz
 } >> /etc/profile
 
 # Install nodejs
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs
 
 # Copy build scripts from Synced Folder to Vagrant machine user home
